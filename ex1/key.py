ff87d79d2d34735cf85d9037cadc6e216550538b
10impossible <134996107+10impossible@users.noreply.github.com>
2023-06-06 02:13:42 +0900

    Update key.py

--- ex1/key.py
+++ ex1/key.py
@@ -43,13 +43,14 @@ def main(args=None):
             time.sleep(5)
 
             # 90도 오른쪽으로 회전하는 동작 추가
+            angle += 90
             twist = Twist()
             angle = float(angle)
-            twist.angular.z = 90
+            twist.angular.z = angle
             pub.publish(twist)
             
             # 다시 속도가 10인 상태로 전진
-            speed += 150
+            speed += 50
             speed = float(speed)
             twist.linear.x = speed
             pub.publish(twist)
