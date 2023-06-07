03aa3240a7f057554189deda1461b62ca2652473
10impossible <134996107+10impossible@users.noreply.github.com>
2023-06-08 01:31:24 +0900

    Update key.py

--- ex1/key.py
+++ ex1/key.py
@@ -310,7 +310,7 @@ def main(args=None):
             twist.linear.x = speed
             twist.angular.z = angle
             pub.publish(twist)
-            time.sleep(6)
+            time.sleep(5)
 
             #180
             angle = 45
